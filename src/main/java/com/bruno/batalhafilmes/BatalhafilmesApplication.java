e36f9a736688b814718848021d6103d533c56781
t.brunoh.supera <brunocompila@hotmail.com>
2022-02-21 23:18:13 -0300

    Criaçao da estrutura do projeto

---
+++ src/main/java/com/bruno/batalhafilmes/BatalhafilmesApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.batalhafilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatalhafilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatalhafilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/batalhafilmes/BatalhafilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.batalhafilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatalhafilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
